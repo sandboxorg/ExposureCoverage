a282feac801e07c7e664af132d52526df2332d15
Shashank Kapadia <kapadia.s@hotmail.com>
2016-01-14 17:34:53 -0500

    Exposure Coverage: Restructured

--- ExposureCoverage/main.py
+++ ExposureCoverage/main.py
@@ -9,99 +9,151 @@ Exposure Coverage
 
 """
 # Import standard Python packages
-
 import warnings
 from os import listdir
 
 import pandas as pd
+import copy
 
 warnings.filterwarnings('ignore')
 
+__author__ = 'Shashank Kapadia'
+__copyright__ = '2015 AIR Worldwide, Inc.. All rights reserved'
+__version__ = '1.0'
+__interpreter__ = 'Anaconda - Python 2.7.10 64 bit'
+__maintainer__ = 'Shashank kapadia'
+__email__ = 'skapadia@air-worldwide.com'
+__status__ = 'Complete'
 
 class ExposureCoverage:
+
+    columns_location = [('LocationID', 'unique'), ('LocationGroups', 'unique'),
+                        ('InceptionExpirationDate', 'unique'), ('CountryISO', 'unique'), ('LatLong', 'unique'),
+                        ('BuildingValue', 'sum'), ('OtherValue', 'sum'), ('ContentsValue', 'sum'),
+                        ('RiskCount', 'unique'), ('Premium', 'unique'),
+                        ('ConstructionCodeType', 'unique'), ('OccupancyCode', 'unique'),
+                        ('LocPerils', 'unique'), ('LocLimitType', 'unique'), ('LimitBldg', 'sum'),
+                        ('LimitOther', 'sum'), ('LimitContent', 'sum'), ('LimitTime', 'sum'),
+                        ('Participation1', 'unique'), ('Participation2', 'unique'), ('DeductType', 'unique'),
+                        ('DeductBldg', 'sum'), ('DeductOther', 'sum'), ('DeductContent', 'sum'),
+                        ('DeductTime', 'sum'), ('SubLimitArea', 'unique')]
+    columns_contract = [('Status', 'unique'), ('Perils', 'unique'), ('LOB', 'unique'), ('LayerPerils', 'unique'),
+                        ('LimitType', 'unique'), ('Limit1', 'sum'), ('LimitA', 'sum'), ('LimitB', 'sum'),
+                        ('LimitC', 'sum'), ('LimitD', 'sum'), ('Limit2', 'sum'), ('DedAmt1', 'sum'),
+                        ('DedAmt2', 'sum'), ('AttachmentAmt', 'sum'), ('DedType', 'unique'),
+                        ('SublimitPerils', 'unique'), ('SublimitArea', 'unique'), ('SubLimitLimitType', 'unique'),
+                        ('SublimitDedType', 'unique'), ('SubLimitOcc', 'sum'), ('SublimitPart', 'sum'),
+                        ('SublimitLimitA', 'sum'), ('SublimitLimitB', 'sum'), ('SublimitLimitC', 'sum'),
+                        ('SublimitLimitD', 'sum'), ('SublimiAttachA', 'sum'), ('SublimiAttachB', 'sum'),
+                        ('SublimiAttachC', 'sum'), ('SublimiAttachD', 'sum'), ('SublimitDedAmt1', 'sum'),
+                        ('SublimitDedAmt2', 'sum')]
+    columns_common = [('ContractID', 'unique'), ('Currency', 'unique')]
+
     def __init__(self, dir_path):
 
         self.path = dir_path
         self.files = [str(self.path + '\\' + filename)
                       for filename in listdir(self.path) if filename.endswith('.csv')]
 
-    def run(self):
+    def summarize_file(self, file, type, data=None):
 
-        masterdata_loc = pd.DataFrame()
-        masterdata_con = pd.DataFrame()
-        latlong = pd.DataFrame()
-        count_loc = 0
-        count_con = 0
-        columns_location = [('LocationID', 'unique'), ('LocationGroups', 'unique'),
-                            ('InceptionExpirationDate', 'unique'), ('CountryISO', 'unique'), ('LatLong', 'unique'),
-                            ('BuildingValue', 'sum'), ('OtherValue', 'sum'), ('ContentsValue', 'sum'),
-                            ('RiskCount', 'unique'), ('Premium', 'unique'),
-                            ('ConstructionCodeType', 'unique'), ('OccupancyCode', 'unique'),
-                            ('LocPerils', 'unique'), ('LocLimitType', 'unique'), ('LimitBldg', 'sum'),
-                            ('LimitOther', 'sum'), ('LimitContent', 'sum'), ('LimitTime', 'sum'),
-                            ('Participation1', 'unique'), ('Participation2', 'unique'), ('DeductType', 'unique'),
-                            ('DeductBldg', 'sum'), ('DeductOther', 'sum'), ('DeductContent', 'sum'),
-                            ('DeductTime', 'sum'), ('SubLimitArea', 'unique')]
-        columns_contract = [('Status', 'unique'), ('Perils', 'unique'), ('LOB', 'unique'), ('LayerPerils', 'unique'),
-                            ('LimitType', 'unique'), ('Limit1', 'sum'), ('LimitA', 'sum'), ('LimitB', 'sum'),
-                            ('LimitC', 'sum'), ('LimitD', 'sum'), ('Limit2', 'sum'), ('DedAmt1', 'sum'),
-                            ('DedAmt2', 'sum'), ('AttachmentAmt', 'sum'), ('DedType', 'unique'),
-                            ('SublimitPerils', 'unique'), ('SublimitArea', 'unique'), ('SubLimitLimitType', 'unique'),
-                            ('SublimitDedType', 'unique'), ('SubLimitOcc', 'sum'), ('SublimitPart', 'sum'),
-                            ('SublimitLimitA', 'sum'), ('SublimitLimitB', 'sum'), ('SublimitLimitC', 'sum'),
-                            ('SublimitLimitD', 'sum'), ('SublimiAttachA', 'sum'), ('SublimiAttachB', 'sum'),
-                            ('SublimiAttachC', 'sum'), ('SublimiAttachD', 'sum'), ('SublimitDedAmt1', 'sum'),
-                            ('SublimitDedAmt2', 'sum')]
-        columns_common = [('ContractID', 'unique'), ('Currency', 'unique')]
+        if data is None:
+            data = pd.DataFrame(pd.read_csv(file, sep=',', header=0))
+        self.data = copy.deepcopy(data)
+        self._getlatlong()
+
+        summary_data = {}
+
+        summary_data['FileName'] = str(file.split('\\')[-1])
+        if type == 'location':
+            columns = self.columns_location + self.columns_common
+            self._get_inception_expiration()
+            summary_data['InceptionExpirationDate'] = self.data['InceptionExpirationDate'].unique()
+            summary_data['TimeElementValue'] = self._get_time_element_value()
+            summary_data['MultiScenario'] = self._get_multiscenario()
+
+        elif type == 'contract':
+            columns = self.columns_contract + self.columns_common
+
+        for clmn in columns:
+            if clmn[1] == 'unique':
+                summary_data[clmn[0]] = self._getunique(clmn[0])
+            elif clmn[1] == 'sum':
+                summary_data[clmn[0]] = self._getsum(clmn[0])
+        if type == 'location':
+            return summary_data, self.data[['Latitude', 'Longitude']]
+        elif type == 'contract':
+            return summary_data
+
+    def run_coverage_dir(self):
+
+        masterdata = [pd.DataFrame(), pd.DataFrame(), pd.DataFrame()]
 
         for file in self.files:
             print(file)
             data = pd.DataFrame(pd.read_csv(file, sep=',', header=0))
             if ('ContractID' and 'LocationID') in data.columns:
-                try:
-                    data['LatLong'] = zip(data.Latitude, data.Longitude)
-                except:
-                    data['LatLong'] = zip(0.0, 0.0)
-                try:
-                    data['InceptionExpirationDate'] = (zip(data.InceptionDate, data.ExpirationDate))
-                except:
-                    data['InceptionExpirationDate'] = 'Incorrect Column headers'
 
-                masterdata_loc.loc[count_loc, 'FileName'] = (file.split('\\')[-1])
-                for column in columns_location + columns_common:
-                    try:
-                        if column[1] == 'unique':
-                            masterdata_loc.loc[count_loc, column[0]] = data[column[0]].unique()
-                        elif column[1] == 'sum':
-                            masterdata_loc.loc[count_loc, column[0]] = data[column[0]].sum()
-                    except:
-                        masterdata_loc.loc[count_loc, column[0]] = ''
+                summary_data, latlong = self.summarize_file(file=file, data=data, type='location')
                 try:
-                    masterdata_loc.loc[count_loc, 'SumTimeElementValue'] = (
-                        (data['TimeElementValue'] / data['DaysCovered']) * 365.0).sum()
+                    summary_data = pd.DataFrame.from_dict(summary_data)
                 except:
-                    masterdata_loc.loc[count_loc, 'SumTimeElementValue'] = ''
+                    summary_data = pd.DataFrame.from_dict(summary_data, orient='index').transpose()
+                masterdata[0] = pd.concat([masterdata[0], summary_data], axis=0)
+                masterdata[2] = pd.concat([masterdata[2], latlong], axis=0)
+
+            elif ('ContractID') in data.columns:
+                summary_data = self.summarize_file(file=file, data=data, type='contract')
                 try:
-                    if data.iloc[:, 126:(126 + 70)].dropna().empty:
-                        masterdata_loc.loc[count_loc, 'MultiTermScenario'] = 'NO'
-                    else:
-                        masterdata_loc.loc[count_loc, 'MultiTermScenario'] = 'YES'
+                    summary_data = pd.DataFrame.from_dict(summary_data)
                 except:
-                    masterdata_loc.loc[count_loc, 'MultiTermScenario'] = 'NO'
-                count_loc += 1
-                latlong = pd.concat([latlong, data[['Latitude', 'Longitude']]], axis=0)
+                    summary_data = pd.DataFrame.from_dict(summary_data, orient='index').transpose()
+                masterdata[1] = pd.concat([masterdata[1], summary_data], axis=0)
 
-            elif 'ContractID' in data.columns:
 
-                masterdata_con.loc[count_con, 'FileName'] = (file.split('\\')[-1])
-                for column in columns_contract + columns_common:
-                    try:
-                        if column[1] == 'unique':
-                            masterdata_con.loc[count_con, column[0]] = data[column[0]].unique()
-                        elif column[1] == 'sum':
-                            masterdata_con.loc[count_con, column[0]] = data[column[0]].sum()
-                    except:
-                        masterdata_con.loc[count_con, column[0]] = ''
-                count_con += 1
+        return masterdata
 
-        return masterdata_loc, masterdata_con, latlong
+    def _getlatlong(self):
+
+        try:
+            self.data['LatLong'] = zip(self.data.Latitude, self.data.Longitude)
+        except:
+            self.data['LatLong'] = ''
+
+    def _get_inception_expiration(self):
+
+        try:
+            self.data['InceptionExpirationDate'] = (zip(self.data.InceptionDate, self.data.ExpirationDate))
+        except:
+            self.data['InceptionExpirationDate'] = 'Incorrect Column headers'
+
+    def _getunique(self, column_name):
+
+        try:
+            return self.data[column_name].unique()
+        except:
+            return ''
+
+    def _getsum(self, column_name):
+
+        try:
+            return self.data[column_name].sum()
+        except:
+            return ''
+
+    def _get_time_element_value(self):
+        try:
+            return ((self.data['TimeElementValue'] /
+                                                 self.data['DaysCovered']) * 365.0).sum()
+        except:
+            return ''
+
+    def _get_multiscenario(self):
+
+        try:
+            if self.data.iloc[:, 126:(126 + 70)].dropna().empty:
+                return 'NO'
+            else:
+                return 'YES'
+        except:
+            return 'NO'
