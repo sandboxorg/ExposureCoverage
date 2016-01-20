6b8f31de48e498e77d4cf9f283d64a0fe08763d8
Shashank Kapadia <kapadia.s@hotmail.com>
2016-01-20 10:55:32 -0500

    Test plan summary: Exposure Coverage library

--- exposurecoverage/exposure_coverage.py
+++ exposurecoverage/exposure_coverage.py
@@ -9,11 +9,14 @@ Exposure Coverage
 
 """
 # Import standard Python packages
+import copy
+import json
+import sys
 import warnings
 from os import listdir
+from urllib2 import urlopen
 
 import pandas as pd
-import copy
 
 warnings.filterwarnings('ignore')
 
@@ -25,8 +28,9 @@ __maintainer__ = 'Shashank kapadia'
 __email__ = 'skapadia@air-worldwide.com'
 __status__ = 'Complete'
 
-class ExposureCoverage:
 
+# noinspection PyBroadException,PyUnresolvedReferences,PyTypeChecker
+class ExposureCoverage:
     columns_location = [('LocationID', 'unique'), ('LocationGroups', 'unique'),
                         ('InceptionExpirationDate', 'unique'), ('CountryISO', 'unique'), ('LatLong', 'unique'),
                         ('BuildingValue', 'sum'), ('OtherValue', 'sum'), ('ContentsValue', 'sum'),
@@ -54,62 +58,62 @@ class ExposureCoverage:
         self.path = dir_path
         self.files = [str(self.path + '\\' + filename)
                       for filename in listdir(self.path) if filename.endswith('.csv')]
+        self.data = ''
 
-    def summarize_file(self, file, type, data=None):
+    def summarize_file(self, file_path, file_type, data=None):
 
         if data is None:
-            data = pd.DataFrame(pd.read_csv(file, sep=',', header=0))
+            data = pd.DataFrame(pd.read_csv(file_path, sep=',', header=0))
         self.data = copy.deepcopy(data)
         self._getlatlong()
 
-        summary_data = {}
+        summary_data = {'FileName': str(file_path.split('\\')[-1])}
 
-        summary_data['FileName'] = str(file.split('\\')[-1])
-        if type == 'location':
+        if file_type == 'location':
             columns = self.columns_location + self.columns_common
             self._get_inception_expiration()
             summary_data['InceptionExpirationDate'] = self.data['InceptionExpirationDate'].unique()
             summary_data['TimeElementValue'] = self._get_time_element_value()
             summary_data['MultiScenario'] = self._get_multiscenario()
 
-        elif type == 'contract':
+        elif file_type == 'contract':
             columns = self.columns_contract + self.columns_common
+        else:
+            sys.exit()
 
         for clmn in columns:
             if clmn[1] == 'unique':
-                summary_data[clmn[0]] = self._getunique(clmn[0])
+                summary_data['Unique' + clmn[0]] = self._getunique(clmn[0])
             elif clmn[1] == 'sum':
-                summary_data[clmn[0]] = self._getsum(clmn[0])
-        if type == 'location':
-            return summary_data, self.data[['Latitude', 'Longitude']]
-        elif type == 'contract':
+                summary_data['Sum' + clmn[0]] = self._getsum(clmn[0])
+
+        if file_type == 'location':
+            try:
+                return summary_data, self.data[['Latitude', 'Longitude']]
+            except:
+                return summary_data, pd.DataFrame(columns=['Latitude', 'Longitude'])
+        elif file_type == 'contract':
             return summary_data
 
     def run_coverage_dir(self):
 
         masterdata = [pd.DataFrame(), pd.DataFrame(), pd.DataFrame()]
 
-        for file in self.files:
-            print(file)
-            data = pd.DataFrame(pd.read_csv(file, sep=',', header=0))
+        for each_file in self.files:
+            print(each_file)
+            data = pd.DataFrame(pd.read_csv(each_file, sep=',', header=0))
             if ('ContractID' and 'LocationID') in data.columns:
 
-                summary_data, latlong = self.summarize_file(file=file, data=data, type='location')
-                try:
-                    summary_data = pd.DataFrame.from_dict(summary_data)
-                except:
-                    summary_data = pd.DataFrame.from_dict(summary_data, orient='index').transpose()
+                summary_data, latlong = self.summarize_file(file_path=each_file, data=data, file_type='location')
+                summary_data = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in summary_data.iteritems()]))
                 masterdata[0] = pd.concat([masterdata[0], summary_data], axis=0)
                 masterdata[2] = pd.concat([masterdata[2], latlong], axis=0)
 
-            elif ('ContractID') in data.columns:
-                summary_data = self.summarize_file(file=file, data=data, type='contract')
-                try:
-                    summary_data = pd.DataFrame.from_dict(summary_data)
-                except:
-                    summary_data = pd.DataFrame.from_dict(summary_data, orient='index').transpose()
-                masterdata[1] = pd.concat([masterdata[1], summary_data], axis=0)
+            elif 'ContractID' in data.columns:
 
+                summary_data = self.summarize_file(file_path=each_file, data=data, file_type='contract')
+                summary_data = pd.DataFrame(dict([(k, pd.Series(v)) for k, v in summary_data.iteritems()]))
+                masterdata[1] = pd.concat([masterdata[1], summary_data], axis=0)
 
         return masterdata
 
@@ -128,9 +132,8 @@ class ExposureCoverage:
             self.data['InceptionExpirationDate'] = 'Incorrect Column headers'
 
     def _getunique(self, column_name):
-
         try:
-            return self.data[column_name].unique()
+            return len(self.data[column_name].unique())
         except:
             return ''
 
@@ -143,8 +146,7 @@ class ExposureCoverage:
 
     def _get_time_element_value(self):
         try:
-            return ((self.data['TimeElementValue'] /
-                                                 self.data['DaysCovered']) * 365.0).sum()
+            return ((self.data['TimeElementValue'] / self.data['DaysCovered']) * 365.0).sum()
         except:
             return ''
 
@@ -157,3 +159,20 @@ class ExposureCoverage:
                 return 'YES'
         except:
             return 'NO'
+
+
+def getplace(lat, lon):
+    url = "http://maps.googleapis.com/maps/api/geocode/json?"
+    url += "latlng=%s,%s&sensor=false" % (lat, lon)
+    v = urlopen(url).read()
+    j = json.loads(v)
+    print(j)
+    components = j['results'][0]['address_components']
+    print(components)
+    country = town = None
+    for c in components:
+        if "country" in c['types']:
+            country = c['long_name']
+        if "postal_town" in c['types']:
+            town = c['long_name']
+    return town, country
